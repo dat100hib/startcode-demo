c093bd9d4292bc841bb235f856ae9cd11ecdc318
Lars Michael Kristensen <lmkr@users.noreply.github.com>
2025-09-11 09:25:01 +0200

    basic setup of startcode demo code

---
+++ Startcodedemo/src/no/hvl/dat100/kalkulator/app/Kalkulator.java
@@ -0,0 +1,32 @@
+package no.hvl.dat100.kalkulator.app;
+
+public class Kalkulator {
+
+    public static int add(int a, int b) {
+
+        return a+b;
+        // TODO
+        // throw new RuntimeException("add ikke implementert");
+    }
+
+    public static int sub(int a, int b) {
+
+        return a-b;
+        // TODO
+        // throw new RuntimeException("sub ikke implementert");
+    }
+
+    public static int mul(int a, int b) {
+
+        return a*b;
+        // TODO
+        // throw new RuntimeException("mul ikke implementert");
+    }
+
+    public static int div(int a, int b) {
+
+        return a/b;
+        // TODO
+        // throw new RuntimeException("div ikke implementert");
+    }
+}
