c9e0d0aaf4e83a1a2d94a87cbc9ad4098a0a9dd0
Lars Michael Kristensen <lmkr@users.noreply.github.com>
2025-09-14 10:17:35 +0200

    fjernet Kalkulator implementasjon

--- src/main/java/no/hvl/dat100/Kalkulator.java
+++ src/main/java/no/hvl/dat100/Kalkulator.java
@@ -4,29 +4,25 @@ public class Kalkulator {
 
     public static int add(int a, int b) {
 
-        return a+b;
         // TODO
-        // throw new RuntimeException("add ikke implementert");
+        throw new RuntimeException("add ikke implementert");
     }
 
     public static int sub(int a, int b) {
 
-        return a-b;
         // TODO
-        // throw new RuntimeException("sub ikke implementert");
+        throw new RuntimeException("sub ikke implementert");
     }
 
     public static int mul(int a, int b) {
 
-        return a*b;
         // TODO
-        // throw new RuntimeException("mul ikke implementert");
+        throw new RuntimeException("mul ikke implementert");
     }
 
     public static int div(int a, int b) {
 
-        return a/b;
         // TODO
-        // throw new RuntimeException("div ikke implementert");
+        throw new RuntimeException("div ikke implementert");
     }
 }
